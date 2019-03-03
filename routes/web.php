20da67fc89a6fd1036b81d0c65c505cd79a1319d
Andi Ganteng <andisyafrianda@gmail.com>
2019-03-03 22:44:22 +0700

    Menggunakan Middleware pada Route (umur)

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
@@ -21,43 +10,13 @@ $router->get('/key', function()
   return str_random(32);
 });
 
-
-$router->get('/user/{id}', function($id)
+// http://lumen-blog.test/admin/home?age=20
+$router->get('admin/home', ['middleware' => 'age', function()
 {
-  return 'User id = '.$id;
-});
-
-$router->get('/post/{postID}/comments/{commentID}', function($postID, $commentID)
-{
-  return 'Post Id = '.$postID.' -- Comment Id = '.$commentID;
-});
-
-
-// ################### Menambahkan [ ] untuk opsional ###################
-$router->get('/params[/{paramId}]', function($paramId = null)
-{
-  return 'Params = '.$paramId;
-});
-
-// ################### Menambahkan aliases + redirect ###################
-$router->get('profile', function()
-{
-    return redirect()->route('route.profile');
-});
-
-$router->get('profile/tes', ['as' => 'route.profile', function() {
-  return route('route.profile');
+  return 'Umurnya cukup';
 }]);
 
-$router->group(['prefix' => 'admin', 'middleware' => 'auth'], function() use($router)
+$router->get('fail', function()
 {
-  $router->get('home', function()
-  {
-    return 'Home Admin';
-  });
-
-  $router->get('profile', function()
-  {
-    return 'Profile Admin';
-  });
+  return 'Umurnya belum om';
 });
