4d41b7e1154e5ecbc435a7a23b6ffa6268d69b52
Andi Ganteng <andisyafrianda@gmail.com>
2019-03-04 16:01:47 +0700

    middleware pada controller

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -11,7 +11,9 @@ class ExampleController extends Controller
      */
     public function __construct()
     {
-        //
+        // $this->middleware('age', ['only' => ['getUser']]);
+        $this->middleware('age', ['except' => ['getUser']]);
+
     }
 
     public function generateKey(){
